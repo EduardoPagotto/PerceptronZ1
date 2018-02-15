374746d4efdf0db2cd198e13bdcf487930b0268b
Eduardo Pagotto <edupagotto@gmail.com>
2018-02-15 19:30:49 -0200

    incio de classe de NeuralNet

--- multilayer.py
+++ multilayer.py
@@ -12,84 +12,8 @@ Update on 20180215
 #pylint: disable=R0913
 
 import numpy as np
-
-def sigmoid(x):
-    '''
-    função sigmoid
-    '''
-    return 1/(1+np.exp(-x))
-
-def sigmoid_derivative(x):
-    '''
-    função sigmoid derivada
-    '''  
-    return x*(1-x)
-
-def tanh(x, derivative=False):
-    if (derivative == True):
-        return (1 - (x ** 2))
-    return np.tanh(x)
-
-def relu(x, derivative=False):
-    if (derivative == True):
-        for i in range(0, len(x)):
-            for k in range(len(x[i])):
-                if x[i][k] > 0:
-                    x[i][k] = 1
-                else:
-                    x[i][k] = 0
-        return x
-    for i in range(0, len(x)):
-        for k in range(0, len(x[i])):
-            if x[i][k] > 0:
-                pass  # do nothing since it would be effectively replacing x with x
-            else:
-                x[i][k] = 0
-    return x
-
-def arctan(x, derivative=False):
-    if (derivative == True):
-        return (np.cos(x) ** 2)
-    return np.arctan(x)
-
-def step(x, derivative=False):
-    if (derivative == True):
-        for i in range(0, len(x)):
-            for k in range(len(x[i])):
-                if x[i][k] > 0:
-                    x[i][k] = 0
-        return x
-    for i in range(0, len(x)):
-        for k in range(0, len(x[i])):
-            if x[i][k] > 0:
-                x[i][k] = 1
-            else:
-                x[i][k] = 0
-    return x
-
-def squash(x, derivative=False):
-    if (derivative == True):
-        for i in range(0, len(x)):
-            for k in range(0, len(x[i])):
-                if x[i][k] > 0:
-                    x[i][k] = (x[i][k]) / (1 + x[i][k])
-                else:
-                    x[i][k] = (x[i][k]) / (1 - x[i][k])
-        return x
-    for i in range(0, len(x)):
-        for k in range(0, len(x[i])):
-            x[i][k] = (x[i][k]) / (1 + abs(x[i][k]))
-    return x
-
-def gaussian(x, derivative=False):
-    if (derivative == True):
-        for i in range(0, len(x)):
-            for k in range(0, len(x[i])):
-                x[i][k] = -2* x[i][k] * np.exp(-x[i][k] ** 2)
-    for i in range(0, len(x)):
-        for k in range(0, len(x[i])):
-            x[i][k] = np.exp(-x[i][k] ** 2)
-    return x
+from ActivactionFunction import sigmoid
+from ActivactionFunction import sigmoid_derivative
 
 def uma_camada():
     
@@ -181,6 +105,7 @@ def duas_camadas():
     print('Saida apos o treino')
     print(l2)
 
+
 def multi_laye_xor():
     #synapses
     syn0 = 2 * np.random.random((3,2)) - 1
@@ -227,7 +152,6 @@ def multi_laye_xor():
         w_bias2 += l3_delta
         w_bias1 += l2_delta
         w_bias0 += l1_delta
-
 
     print('Teste Final.....')
 
